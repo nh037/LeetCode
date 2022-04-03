2c9520def29e5fda330075b9348482d262973482
Nitish Hans <60508644+nh037@users.noreply.github.com>
2022-04-03 14:28:06 +0530

    Time: 114 ms (5.01%), Space: 16.2 MB (99.39%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -4,14 +4,20 @@ private:
     while (i < j) {
          i++, j--;  
         if (s[i] != s[j]) {
-          return false; } }
-    return true; }
+          return false; 
+        } 
+    }
+    return true; 
+    }
 public:        
 bool validPalindrome(string& s) {
     int i = -1; int j = s.length();        
     while (i < j) {
          i = i + 1, j = j - 1; 
         if (s[i] != s[j]) {
-          return (isItPalindrome(s, i, j + 1) or isItPalindrome(s, i - 1, j)); } }  
-    return true; } 
+          return (isItPalindrome(s, i, j + 1) or isItPalindrome(s, i - 1, j)); 
+        } 
+    }  
+    return true; 
+   } 
 };
