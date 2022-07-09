6b853b4e25ad1afa2a95cc88db93ade7d2d9a2db
Nitish Hans <60508644+nh037@users.noreply.github.com>
2022-07-09 14:32:19 +0530

    Time: 11 ms (38.00%), Space: 8.6 MB (21.13%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> res(n+1, 0);
-        for (int i = 1; i <= n; ++i) {
-            res[i] = res[i >> 1] + (i & 1);
+        vector<int>result;
+        for(int i=0;i<=n;i++){
+            result.push_back(getNoofSetBits(i));           
         }
-        return res;
-
+        return result;
+    }
+    int getNoofSetBits(int num){
+        int count=0;
+        for(int i=0;i<=31;i++){
+            if((num & (1<<i))>0){
+                count++;
+            }
+        }
+        return count;
     }
 };
